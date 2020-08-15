efef7e4a5e49fc02c85680b9f31b26b380fe907a
parentsmyworld <madhu@skylinkmsc.com>
2020-08-15 18:07:56 +0300

    UI Completed

---
+++ android/app/src/main/kotlin/com/logixspace/flutter_1stassignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.logixspace.flutter_1stassignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("STUDENT APP"),
+        ),
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20.0,),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Name",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.keyboard),
+                ),
+              ),
+              SizedBox(height:20.0,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.keyboard),
+                  hintText: "Enter Roll No."
+                ),
+              ),
+              SizedBox(height:20.0,),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.keyboard),
+                    hintText: "Admission No."
+                ),
+              ),
+              SizedBox(height:20.0,),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.keyboard),
+                    hintText: "College Name"
+                ),
+              ),
+              Center(
+                child: RaisedButton(
+                    onPressed: () => {},
+                    child: Text("SUBMIT"),
+                    color: Colors.green),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
