442e007439fc0ce68487d54e86c933a01a6b3001
parentsmyworld <madhu@skylinkmsc.com>
2020-08-16 16:02:06 +0300

    taking user input

--- lib/main.dart
+++ lib/main.dart
@@ -6,56 +6,77 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  TextEditingController name=TextEditingController();
+  TextEditingController rollno=TextEditingController();
+  TextEditingController admno=TextEditingController();
+  TextEditingController collname=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("STUDENT APP"),
+          title: Center(child: Text("STUDENT APP")),
         ),
-        body: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20.0,),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter Name",
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.keyboard),
-                ),
-              ),
-              SizedBox(height:20.0,),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.keyboard),
-                  hintText: "Enter Roll No."
-                ),
-              ),
-              SizedBox(height:20.0,),
-              TextField(
-                decoration: InputDecoration(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20.0,),
+                TextField(
+                  controller: name,
+                  decoration: InputDecoration(
+                    hintText: "Enter Name",
                     border: OutlineInputBorder(),
                     prefixIcon: Icon(Icons.keyboard),
-                    hintText: "Admission No."
+                  ),
                 ),
-              ),
-              SizedBox(height:20.0,),
-              TextField(
-                decoration: InputDecoration(
+                SizedBox(height:20.0,),
+                TextField(
+                  controller: rollno,
+                  decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.keyboard),
-                    hintText: "College Name"
+                      prefixIcon: Icon(Icons.keyboard),
+                    hintText: "Enter Roll No."
+                  ),
                 ),
-              ),
-              Center(
-                child: RaisedButton(
-                    onPressed: () => {},
-                    child: Text("SUBMIT"),
-                    color: Colors.blue),
-              )
-            ],
+                SizedBox(height:20.0,),
+                TextField(
+                  controller: admno,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.keyboard),
+                      hintText: "Admission No."
+                  ),
+                ),
+                SizedBox(height:20.0,),
+                TextField(
+                  controller: collname,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.keyboard),
+                      hintText: "College Name"
+                  ),
+                ),
+                Center(
+                  child: RaisedButton(
+                      onPressed: (){
+                        var a=name.text;
+                        var b=rollno.text;
+                        var c=admno.text;
+                        var d=collname.text;
+                        print(a);
+                        print(b);
+                        print(c);
+                        print(d);
+                        },
+                      child: Text("SUBMIT"),
+                      color: Colors.blue),
+                )
+              ],
+            ),
           ),
         ),
       ),
